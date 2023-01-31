0afe3979aa5d00a18024e2b8b68250052328667f
LosnahDev <124073557+LosnahDev@users.noreply.github.com>
2023-02-01 01:59:34 +0900

    Losnah
    대화 씬

---
+++ Assets/Script/BackGroundBlack.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGroundBlack : MonoBehaviour
+{
+    /*
+    float time;    
+    private void Start()
+    {
+        if (time < 0.5f)
+        {
+            GetComponent<Image>().color = new Color(1, 1, 1, 1 - time);
+        }
+        else
+        { 
+            GetComponent<Image>().color = new Color(1, 1, 1, time);
+
+            if (time > 1f)
+                time = 0;
+        }
+        time += Time.deltaTime;
+    }
+    */
+}
